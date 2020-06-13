cdd93ad47d1d9f6eda8152bb85f871c4b78fc5e5
biagemc <biagemc@gmail.com>
2020-06-13 10:53:35 -0400

    add Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,41 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let charsA = {};
+  let charsB = {};
+  let newA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  let newB = stringB.replace(/[^\w]/g, "").toLowerCase();
 
+  for (let char of newA) {
+    charsA[char] = charsA[char] + 1 || 1;
+  }
+  for (let char of newB) {
+    charsB[char] = charsB[char] + 1 || 1;
+  }
+  console.log(charsA);
+  console.log(charsB);
+  if (Object.keys(charsA).length !== Object.keys(charsB).length) {
+    return false;
+  }
+  for (let char in charsA) {
+    if (charsA[char] !== charsB[char]) {
+      return false;
+    }
+  }
+  return true;
+}
+anagrams("hello", "llohe");
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   let newA = stringA.replace(/[^\w]/g, "").toLowerCase().split("").sort();
+
+//   let newB = stringB.replace(/[^\w]/g, "").toLowerCase().split("").sort();
+
+//   if (newA.join("") === newB.join("")) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
